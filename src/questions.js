7617e5e6a53122f4b41ef096fabd7ffd2ed74206
Chirag Shah <chiragshah321@gmail.com>
2017-08-17 21:15:58 +0000

    first commit

--- src/questions.js
+++ src/questions.js
@@ -256,250 +256,243 @@ module.exports = {
     ],
     "QUESTIONS_EN_US" : [
         {
-            "Reindeer have very thick coats, how many hairs per square inch do they have?": [
-                "13,000",
-                "1,200",
-                "5,000",
-                "700",
-                "1,000",
-                "120,000"
+            "What team won the very first NBA game?": [
+                "New York Knicks",
+                "Toronto Huskies",
+                "Philadelphia Warriors",
+                "Chicago Stags",
             ]
         },
         {
-            "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in. ": [
-                "Japan",
-                "United States",
-                "Finland",
-                "Germany",
-                "Canada",
-                "Norway",
-                "France"
+            "What NBA player scored 100 points on March 2, 1962? ": [
+                "Wilt Chamberlain",
+                "Elgin Baylor",
+                "Bill Russell",
+                "Kareem Abdul-Jabaar",
             ]
         },
         {
-            "Santas reindeer are cared for by one of the Christmas elves, what is his name?": [
-                "Wunorse Openslae",
-                "Alabaster Snowball",
-                "Bushy Evergreen",
-                "Pepper Minstix"
+            "Who was the first player in NBA history to be elected league MVP by a unanimous vote?": [
+                "Stephen Curry",
+                "Michael Jordan",
+                "Magic Johnson",
+                "Lebron James"
             ]
         },
         {
-            "If all of Santas reindeer had antlers while pulling his Christmas sleigh, they would all be": [
-                "Girls",
-                "Boys",
-                "Girls and boys",
-                "No way to tell"
+            "What new kind of shot did Joe Fulks score a record 63 points with in one game in 1949?": [
+                "Jump Shot",
+                "Lay Up",
+                "Three Point Shot",
+                "Free Throw"
             ]
         },
         {
-            "What do Reindeer eat?": [
-                "Lichen",
-                "Grasses",
-                "Leaves",
-                "Berries"
+            "Who scored the first three-point basket in NBA history?": [
+                "Chris Ford",
+                "Gene Stump",
+                "Wes Unseld",
+                "Larry Bird"
             ]
         },
         {
-            "What of the following is not true?": [
-                "Caribou live on all continents",
-                "Both reindeer and Caribou are the same species",
-                "Caribou are bigger than reindeer",
-                "Reindeer live in Scandinavia and Russia"
+            "Who was the youngest player to score 10,000 points in the NBA?": [
+                "Lebron James",
+                "Michael Jordan",
+                "Wilt Chamberlain",
+                "Kobe Bryant"
             ]
         },
         {
-            "In what year did Rudolph make his television debut?": [
-                "1964",
-                "1979",
-                "2000",
-                "1956"
+            "What team owns the longest winning streak in NBA history?": [
+                "Los Angeles Lakers",
+                "Orlando Magic",
+                "Chicago Bulls",
+                "Miami Heat"
             ]
         },
         {
-            "Who was the voice of Rudolph in the 1964 classic?": [
-                "Billie Mae Richards",
-                "Burl Ives",
-                "Paul Soles",
-                "Lady Gaga"
+            "What player was known as the Clown Prince of the Harlem Globetrotters?": [
+                "Meadowlark Lemon",
+                "Goose Tatum",
+                "Marques Haynes",
+                "Fred Curly Neal"
             ]
         },
         {
-            "In 1939 what retailer used the story of Rudolph the Red Nose Reindeer?": [
-                "Montgomery Ward",
-                "Sears",
-                "Macys",
-                "Kmart"
+            "Who is the all-time leading scorer in men's college basketball?": [
+                "Pete Maravich",
+                "Stephen Curry",
+                "Freeman Williams",
+                "Larry Bird"
             ]
         },
         {
-            "Santa\'s reindeer named Donner was originally named what?": [
-                "Dunder",
-                "Donny",
-                "Dweedle",
-                "Dreamy"
+            "Who was the first WNBA player to dunk in a playoff game?": [
+                "Brittney Griner",
+                "Lisa Leslie",
+                "Michelle Snow",
+                "Tamika Catchings"
             ]
         },
         {
-            "Who invented the story of Rudolph?": [
-                "Robert May",
-                "Johnny Marks",
-                "Santa",
-                "J.K. Rowling"
+            "Which NBA coach appeared on Late Night with David Letterman as an eighth grader?": [
+                "Frank Vogel",
+                "Kevin Mchale",
+                "Mark Jackson",
+                "Maurice Cheeks"
             ]
         },
         {
-            "In what location will you not find reindeer?": [
-                "North Pole",
-                "Lapland",
-                "Korvatunturi mountain",
-                "Finland"
+            "How many games did Wilt Chamberlain foul out of during his 14 year NBA career?": [
+                "0",
+                "109",
+                "25",
+                "11"
             ]
         },
         {
-            "What Makes Santa\'s Reindeer Fly?": [
-                "Magical Reindeer Dust",
-                "Fusion",
-                "Amanita muscaria",
-                "Elves"
+            "What player won All-Star Game MVP, NBA MVP, and NBA Finals MVP awards in 2000?": [
+                "Shaquille O'Neal",
+                "Michael Jordan",
+                "Tim Duncan",
+                "Kobe Bryant"
             ]
         },
         {
-            "Including Rudolph, how many reindeer hooves are there?": [
-                "36",
-                "24",
-                "16",
-                "8"
+            "What NBA player retired unexpectedly on November 7, 1991?": [
+                "Magic Johnson",
+                "Michael Jordan",
+                "Moses Malone",
+                "Larry Bird"
             ]
         },
         {
-            "Santa only has one female reindeer. Which one is it?": [
-                "Vixen",
-                "Clarice",
-                "Cupid",
-                "Cupid"
+            "What team did Wilt Chamberlain finish his NBA career with?": [
+                "Los Angeles Lakers",
+                "Chicago Bulls",
+                "Orlando Magic",
+                "Philadelphia Sixers"
             ]
         },
         {
-            "In the 1964 classic Rudolph The Red Nosed Reindeer, what was the snowman narrators name?": [
-                "Sam",
-                "Frosty",
-                "Burl",
-                "Snowy"
+            "Who won the Nestle Crunch Slam Dunk Contest on February 6, 1988?": [
+                "Michael Jordan",
+                "Spud Webb",
+                "Dominque Wilkins",
+                "Magic Johnson"
             ]
         },
         {
-            "What was Rudolph\'s father\'s name?": [
-                "Donner",
-                "Dasher",
-                "Blixen",
-                "Comet"
+            "Who sank a 60-foot shot on April 29, 1970?": [
+                "Jerry West",
+                "Wilt Chamberlain",
+                "Willis Reed",
+                "Kareem Abdul-Jabaar"
             ]
         },
         {
-            "In the 1964 movie, What was the name of the coach of the Reindeer Games?": [
-                "Comet",
-                "Blixen",
-                "Donner",
-                "Dasher"
+            "Who was the first Chinese player to play in an NBA game?": [
+                "Wang Zhizhi",
+                "Hai Rui",
+                "Mao Zedong",
+                "Yao Ming"
             ]
         },
         {
-            "In the 1964 movie, what is the name of the deer that Rudolph befriends at the reindeer games?": [
-                "Fireball",
-                "Clarice",
-                "Jumper",
-                "Vixen"
+            "What piece of basketball equipment formerly owned by Stephen Curry sold at auction for $3,190 on August 21, 2016?": [
+                "Mouthguard",
+                "Tennis Shoes",
+                "Headband",
+                "Shoe Laces"
             ]
         },
         {
-            "In the 1964 movie, How did Donner, Rudolph\'s father, try to hide Rudolph\'s nose?": [
-                "Black mud",
-                "Bag",
-                "Pillow case",
-                "Sock"
+            "Why did Kevin Durant choose the jersey number 35?": [
+                "His first coach was murdered at 35",
+                "He plans to play until he is 35",
+                "It's the first tetrahedral number",
+                "His hero was reggie lewis"
             ]
         },
         {
-            "In the 1964 movie, what does the Misfit Elf want to be instead of a Santa Elf?": [
-                "Dentist",
-                "Reindeer",
-                "Toy maker",
-                "Candlestick maker"
+            "What team drafted Pau Gasol in the 2001 NBA draft?": [
+                "Atlanta Hawks",
+                "Memphis Grizzlies",
+                "San Antonion Spurs",
+                "Los Angeles Lakers"
             ]
         },
         {
-            "In the 1964 movie,what was the Bumble\'s one weakness?": [
-                "Could not swim",
-                "Always hungry",
-                "Candy canes",
-                "Cross eyed"
+            "What NBA player has won the most league MVPs?": [
+                "Kareem Abdul-Jabaar",
+                "Michael Jordan",
+                "Stephen Curry",
+                "Lebron James"
             ]
         },
         {
-            "In the 1964 movie, what is Yukon Cornelius really in search of?": [
-                "Peppermint",
-                "Gold",
-                "India",
-                "Polar Bears"
+            "What teammates were nicknamed the Splash Brothers?": [
+                "Klay Thompson and Stephen Curry",
+                "Michael Jordan and Scottie Pippen",
+                "Jery West and Wilt Chamberlain",
+                "Kevin Durant and Russell Westbrook"
             ]
         },
         {
-            "In the 1964 movie, why is the train on the Island of Misfit Toys?": [
-                "Square wheels",
-                "No Engine",
-                "Paint does not match",
-                "It does not toot"
+            "How many points did LeBron James score in his first NBA game?": [
+                "25",
+                "41",
+                "9",
+                "15"
             ]
         },
         {
-            "In the 1964 movie, what is the name of the Jack in the Box?": [
-                "Charlie",
-                "Sam",
-                "Billy",
-                "Jack"
+            "Who was the shortest player in NBA history?": [
+                "Muggsy Bogues",
+                "Spud Webb",
+                "Earl Boykins",
+                "Manute Bol"
             ]
         },
         {
-            "In the 1964 movie, why did Santa Claus almost cancel Christmas?": [
-                "Storm",
-                "No snow",
-                "No toys",
-                "The Reindeer were sick"
+            "What NBA player was nicknamed The Glide?": [
+                "Clyde Drexler",
+                "Allen Iverson",
+                "Dikembe Mutombo",
+                "Carmelo Anthony"
             ]
         },
         {
-            "In the 1964 movie, what animal noise did the elf make to distract the Bumble?": [
-                "Oink",
-                "Growl",
-                "Bark",
-                "Meow"
+            "How many players were allowed on the court per team in the first college basketball game?": [
+                "9",
+                "11",
+                "3",
+                "5"
             ]
         },
         {
-            "In the 1964 movie, what is the name of the prospector?": [
-                "Yukon Cornelius",
-                "Slider Sam",
-                "Bumble",
-                "Jack"
+            "Why did Michael Jordan wear number 45 when he came out of retirement?": [
+                "All of these",
+                "His Brother wore 45 in high school",
+                "His number 23 had already been retired",
+                "He wore 45 playing minor league baseball"
             ]
         },
         {
-            "How far do reindeer travel when they migrate?": [
-                "3000 miles",
-                "700 miles",
-                "500 miles",
-                "0 miles"
+            "Who invented the game of basketball?": [
+                "James Naismith",
+                "Frank Mahan",
+                "Abner Doubleday",
+                "Walter Camp"
             ]
         },
         {
-            "How fast can a reindeer run?": [
-                "48 miles per hour",
-                "17 miles per hour",
-                "19 miles per hour",
-                "14 miles per hour",
-                "52 miles per hour",
-                "41 miles per hour"
+            "What team won the first National Basketball League championship?": [
+                "Trenton Nationals",
+                "New York Wanderers",
+                "Camden Electrics",
+                "Bristol Pile Drivers",
             ]
         }
     ],
